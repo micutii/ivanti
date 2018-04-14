3cbd0885fcc2a36ba9e4f3039f9a5d2c1d5ed1aa
DESKTOP-8MH5BH3\Alexandra <micutii333@gmail.com>
2018-04-14 17:40:35 +0300

    more clients

---
+++ Server/Server/TCP/TrojanClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.TCP
+{
+    public class TrojanClient
+    {
+        public TcpClient Client { get; set; }
+        public string Name { get; set; }
+
+        public TrojanClient(TcpClient tcpClient, string name)
+        {
+            Client = tcpClient;
+            Name = name;
+        }
+    }
+}
--- Server/Server/UI/Window1.xaml.cs
+++ Server/Server/UI/Window1.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Shapes;
 
 namespace Server.UI
 {
+
+    enum CommandsEnum { CmdInjection = 0, ExecProcess = 1, CreateFile = 2, ReadFile = 3, DeleteFile = 4, MouseInvert = 5, DisplayRotate = 6, OsMessage = 7 };
     /// <summary>
     /// Interaction logic for Window1.xaml
     /// </summary>
@@ -24,16 +26,12 @@ namespace Server.UI
         public Window1()
         {
             InitializeComponent();
+            SetCommand();
         }
 
         public void SetLabel(string content)
         {
             label.Content = content;
-        }
-
-        public string GetMessage()
-        {
-            return textBox.Text;
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
@@ -43,13 +41,45 @@ namespace Server.UI
 
         protected virtual void OnSentPressed(EventArgs e)
         {
-            //EventHandler handler = SentPressed;
-            //if (handler != null)
-            //{
-            //    handler(this, e);
-            //}
-
             SentPressed?.Invoke(this, e);
+        }
+
+        public int GetClientId()
+        {
+            return clientsComboBox.SelectedIndex;
+        }
+
+        public int GetCommandId()
+        {
+            return commandComboBox.SelectedIndex;
+        }
+
+
+        public string GetParameters()
+        {
+            return parametersTextBox.Text;
+        }
+
+        private void SetCommand()
+        {
+            foreach (CommandsEnum command in Enum.GetValues(typeof(CommandsEnum)))
+                commandComboBox.Items.Add(command.ToString());
+        }
+
+        public void SetClients(IEnumerable<string> clientNames)
+        {
+            foreach (var clientName in clientNames)
+                clientsComboBox.Items.Add(clientName);
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
