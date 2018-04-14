e911677edf5ee8fc2f1f5828e64f0f751a1092eb
blanzeanudoru <blanzeanu.doru@gmail.com>
2018-04-14 13:33:12 +0300

    Added file manipulation functionality.

--- Client/ClientCpp/Client.h
+++ Client/ClientCpp/Client.h
@@ -1,8 +1,24 @@
 #pragma once
+
+
 class Client
 {
 public:
 	Client();
+	//Process Execution
+	void startProcess(const std::string &);
+	void runCmdCommand(const std::string &);
+
+	//File Handling
+	std::string readFile(const std::string &);
+	bool writeFile(const std::string &, const std::string &);
+	bool deleteFile(const std::string &);
+
+	//Funny stuff
+	void invertMouse();
+	void rotateDisplay();
+	void message();
+
 	~Client();
 
 private:
